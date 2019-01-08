a274b08ed450a8b44514b49eac8350a6bc7e108d
Sanchit Sharma <sanchit.sharma99@gmail.com>
2019-01-08 12:33:21 +0530

    #FirstCommit setup ExecuteInEditMode complete.

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+
+public class EditorSnap : MonoBehaviour {
+    [SerializeField] [Range(1f,20f)] float gridSize = 10f;
+   void Update()
+     {
+        Vector3 snapPOS;
+        snapPOS.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPOS.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize; 
+
+        transform.position = new Vector3(snapPOS.x, 0f, snapPOS.z);
+     }
+    
+}
