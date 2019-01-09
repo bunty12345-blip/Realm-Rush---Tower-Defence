73e72342363d93499530e0efc5287a02a80605e2
Sanchit Sharma <sanchit.sharma99@gmail.com>
2019-01-09 15:16:07 +0530

    Using text mesh for Labels - position grids working

--- Assets/CubeSnap.cs
+++ Assets/CubeSnap.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 [ExecuteInEditMode]
-
-public class EditorSnap : MonoBehaviour {
+[SelectionBase]
+public class CubeSnap : MonoBehaviour {
     [SerializeField] [Range(1f,20f)] float gridSize = 10f;
-   void Update()
+
+    TextMesh textMesh;
+
+     void Update()
      {
         Vector3 snapPOS;
         snapPOS.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPOS.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize; 
-
         transform.position = new Vector3(snapPOS.x, 0f, snapPOS.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPOS.x/gridSize + "," + snapPOS.z/gridSize ;
+
+
      }
+
+ 
     
 }
